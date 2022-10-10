eba3900f229ceb6a5d9d03f1207f6e45eb6c950a
UnixBear <unixbear@gmail.com>
2022-10-10 08:55:27 -0400

    guessing game made

--- 985_Sum_of_Even_Numbers_After_Queries/sum_even_numbers/src/main.rs
+++ 985_Sum_of_Even_Numbers_After_Queries/sum_even_numbers/src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use rand::{Rng, thread_rng};
+use std::cmp::Ordering;
+pub struct Solution {
+    rtn_val:i32,
+}
+impl Solution {
+    pub fn sum_even_after_queries(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+        unimplemented!();
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    println!("fkn guess");
+    let mut sekrit_gen = thread_rng();
+    let sekrit_num :u32 = sekrit_gen.gen_range(0..100);
+    println!("psst the answer is {}", sekrit_num);
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("u fkd up");
+        let guess :u32 = guess.trim().parse().expect("dude this isn't a fkn number try again >;|");
+
+         println!("u guessed: {}", guess);
+        //println!("now we cumpair hehe: {}", guess.cmp(&sekrit_num))
+        match guess.cmp(&sekrit_num) {
+            Ordering::Less => println!("go higher, fuckface"),
+            Ordering::Greater => println!("go lower, fuckface"),
+            Ordering::Equal => {
+                println!("go fuck yourself");
+                break;
+            }
+            
+        }
+    }
 }
