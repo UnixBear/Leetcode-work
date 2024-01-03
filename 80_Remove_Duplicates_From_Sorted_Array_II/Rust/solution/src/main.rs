d2399a13efb65b656a3fe6df6d8ea892f16d4bef
UnixBear <unixbear@gmail.com>
2024-01-03 17:11:59 -0500

    finished rust solution

--- 80_Remove_Duplicates_From_Sorted_Array_II/Rust/solution/src/main.rs
+++ 80_Remove_Duplicates_From_Sorted_Array_II/Rust/solution/src/main.rs
@@ -1,3 +1,10 @@
+mod solution;
+use solution::Solution;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut nums = vec![0,0,1,1,1,1,2,3,3];
+    let k = Solution::remove_duplicates(&mut nums);
+    println!("The length of the array after removing duplicates is: {}", k);
+    println!("The first {} elements of the modified array are: {:?}", k, &nums[..k as usize]);
 }
