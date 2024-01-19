5ac0c6319d7de925f1f401a13708366d365b6ad2
UnixBear <unixbear@gmail.com>
2024-01-18 23:08:18 -0500

    finished rust solution using vec.windows()

--- 122_Best_Time_to_Buy_and_Sell_Stocks_II/Rust/solution/src/main.rs
+++ 122_Best_Time_to_Buy_and_Sell_Stocks_II/Rust/solution/src/main.rs
@@ -1,3 +1,8 @@
+mod solution;
+use solution::Solution;
+
 fn main() {
-    println!("Hello, world!");
+    let test = vec![1,2,3,4,5];
+    let answer = Solution::max_profit(test);
+    println!("The correct answer should be 4.  The result is: {}", answer)
 }
