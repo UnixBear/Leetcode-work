e5c7a38129a211b283a77d505239203275edadca
UnixBear <unixbear@gmail.com>
2024-02-01 23:36:46 -0500

    refactored rust solution to be more rust idiomatic to use an iterator and borrowed nums instead of consuming it

--- 55_Jump_Game/Rust/solution/src/main.rs
+++ 55_Jump_Game/Rust/solution/src/main.rs
@@ -4,7 +4,10 @@ use solution::Solution;
 fn main() {
     let test_one = vec!(2,3,1,1,4);
     let test_two = vec!(3,2,1,0,4);
-    
-    println!("The first test of (2,3,1,1,4) should return true: {}", Solution::can_jump(test_one));
-    println!("The first test of (3,2,1,0,4) should return true: {}", Solution::can_jump(test_two));
+    println!("testing first function");
+    println!("The first test of (2,3,1,1,4) should return true: {}", Solution::can_jump(&test_one));
+    println!("The first test of (3,2,1,0,4) should return false: {}", Solution::can_jump(&test_two));
+    println!("testing second function");
+    println!("The first test of (2,3,1,1,4) should return true: {}", Solution::can_jump2(&test_one));
+    println!("The first test of (3,2,1,0,4) should return false: {}", Solution::can_jump2(&test_two));
 }
